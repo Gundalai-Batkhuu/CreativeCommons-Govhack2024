7b858fed8190576501d6ef29e394dc58ad31d16c
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-09-09 17:15:13 +1000

    quick fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import SearchPageContent from '@/components/search-page-content'
-import { auth } from '@/auth'
-import { Session } from '@/lib/types'
 
 export default async function SearchPage() {
-  const session = (await auth()) as Session
-  const userId = session.user.id
-
+  const userId = '1234'
   return (
     <SearchPageContent userId={userId} />
   )
